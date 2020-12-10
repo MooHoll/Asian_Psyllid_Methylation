c78f509048620405c474fed3d83f154bab033b8f
Hollie Marshall <hollie_marshall@hotmail.co.uk>
2020-12-10 11:50:35 +0000

    fixed typo

--- Differential_expression/03_trimming.sh
+++ Differential_expression/03_trimming.sh
@@ -5,7 +5,7 @@
 
 set -e
 
-SCRATCH=/scratch/$USER/$JOB_ID
+SCRATCH=/scratch/$USER
 mkdir -p $SCRATCH
 cd $SCRATCH
 
@@ -16,16 +16,17 @@ start=`date +%s`
 echo "copying data in"
 rsync /data/ross/misc/analyses/asian_psyllid/raw_data/RNA-Seq/*.fq.gz ./
 
+
 echo "doing the shiz"
 for file in $(ls *1.clean.fq.gz)
 do
 	base=$(basename $file "_1.clean.fq.gz")
 	cutadapt -j 10 \
 	-u 10 -U 10 \
-	-o trim_${base}1.fq.gz \
-	-p trim_${base}2.fq.gz \
-	${base}1.fq.gz \
-	${base}2.fq.gz
+	-o trim_${base}_1.fq.gz \
+	-p trim_${base}_2.fq.gz \
+	${base}_1.clean.fq.gz \
+	${base}_2.clean.fq.gz
 done
 
 echo "moving outputs"
