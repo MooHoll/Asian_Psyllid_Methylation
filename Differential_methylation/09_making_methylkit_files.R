1b679446a29b703d3d0d0633624765ca06f12a29
Hollie Marshall <hollie_marshall@hotmail.co.uk>
2021-01-28 11:40:17 +0000

    making methylkit files

--- Differential_methylation/09_making_methylkit_files.R
+++ Differential_methylation/09_making_methylkit_files.R
@@ -1,9 +1,9 @@
 library(methylKit)
 library(readr)
 
-file.list <- list("F1-1_BDLM200001629-1A.bam", "F1-2_BDLM200001629-1A.bam",
-                  "F1-3_BDLM200001629-1A.bam", "M1-1_BDLM200001632-1A.bam",
-                  "M1-2_BDLM200001629-1A.bam", "M1-3_BDLM200001629-1A.bam")
+file.list <- list("F1-1_BDLM200001629-1A.bam", "F1-2_BDLM200001630-1A.bam",
+                  "F1-3_BDLM200001631-1A.bam", "M1-1_BDLM200001632-1A.bam",
+                  "M1-2_BDLM200001633-1A.bam", "M1-3_BDLM200001634-1A.bam")
 
 raw_data <- processBismarkAln(file.list,
                               sample.id = list("F1","F2","F3","M1","M2","M3"),
